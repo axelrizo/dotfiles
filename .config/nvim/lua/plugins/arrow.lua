3df982e3bee21b4140fe955e338c2bced0c2ef0c
Axel Rizo <axelg.rizos@gmail.com>
2026-04-11 03:05:49 -0500

    fix: update leader_key in arrow.nvim configuration

--- .config/nvim/lua/plugins/arrow.lua
+++ .config/nvim/lua/plugins/arrow.lua
@@ -9,7 +9,7 @@ return {
   config = function()
     require('arrow').setup {
       show_icons = true,
-      leader_key = ';', -- Recommended to be a single key
+      leader_key = '<leader>i', -- Recommended to be a single key
       buffer_leader_key = 'm', -- Per Buffer Mappings
     }
 
