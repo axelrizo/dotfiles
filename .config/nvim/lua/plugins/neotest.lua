262ef51c701995b180d5302668eedc4fbfca02a1
Axel Rizo <axelg.rizos@gmail.com>
2026-04-13 17:42:28 -0500

    feat: add testing keymaps

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -8,8 +8,37 @@ return {
     'jfpedroza/neotest-elixir',
   },
   config = function()
-    require('neotest').setup {
-      adapters = { require 'neotest-elixir' },
-    }
+    neotest = require 'neotest'
+
+    neotest.setup { adapters = { require 'neotest-elixir' } }
+
+    -- summary
+    vim.keymap.set('n', '<leader>tt', function() neotest.summary.toggle { enter = true } end, { desc = '[t]oggle test summary' })
+
+    -- running tests
+    vim.keymap.set('n', '<leader>te', function() neotest.run.run() end, { desc = 'Run n[e]arest test' })
+    vim.keymap.set('n', '<leader>ta', function() neotest.run.run { suite = true } end, { desc = 'Run [a]ll tests' })
+    vim.keymap.set('n', '<leader>tl', function() neotest.run.run_last() end, { desc = 'Run [l]ast test' })
+    vim.keymap.set('n', '<leader>tT', function() neotest.run.stop() end, { desc = '[T]erminate test' })
+    vim.keymap.set('n', '<leader>tc', function() neotest.run.run(vim.fn.expand '%') end, { desc = '[c]urrent file' })
+    vim.keymap.set('n', '<leader>tm', function() neotest.summary.run_marked() end, { desc = 'Run [m]arked tests' })
+
+    -- output
+    vim.keymap.set('n', '<leader>to', function() neotest.output.open { enter = true, auto_close = true } end, { desc = 'Show test [o]utput' })
+    vim.keymap.set('n', '<leader>tO', function() neotest.output_panel.toggle() end, { desc = 'Toggle [O]utput panel' })
+    vim.keymap.set('n', '<leader>tA', function() neotest.run.attach() end, { desc = '[A]ttach to running test' })
+
+    -- jumping
+    vim.keymap.set('n', '<leader>tn', function() neotest.jump.next() end, { desc = '[n]ext test' })
+    vim.keymap.set('n', '<leader>tp', function() neotest.jump.prev() end, { desc = '[p]revious test' })
+    vim.keymap.set('n', '<leader>tN', function() neotest.jump.next { status = 'failed' } end, { desc = '[N]ext failing test' })
+    vim.keymap.set('n', '<leader>tP', function() neotest.jump.prev { status = 'failed' } end, { desc = '[P]revious failing test' })
+
+    -- debugging
+    vim.keymap.set('n', '<leader>td', function()
+      neotest.summary.close()
+      neotest.output_panel.close()
+      neotest.run.run { suite = false, strategy = 'dap' }
+    end, { desc = 'Debug nearest test' })
   end,
 }
