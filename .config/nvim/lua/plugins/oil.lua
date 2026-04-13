b8dd1b5b4fe456dc384c39b63cd8db55da553a80
Axel Rizo <axelg.rizos@gmail.com>
2026-04-13 17:34:54 -0500

    feat: add git status in oil and add winbar to oil buffers

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,27 +1,50 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-  config = function()
-    require('oil').setup {
-      default_file_explorer = true,
-      delete_to_trash = true,
-      skip_confirm_for_simple_edits = true,
-      view_options = {
-        show_hidden = true,
-        natural_order = true,
-        is_always_hidden = function(name, _) return name == '..' or name == '.git' end,
-      },
-      win_options = {
-        wrap = true,
-      },
-    }
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-  end,
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      function _G.get_oil_winbar()
+        local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+        local dir = require('oil').get_current_dir(bufnr)
+        if dir then
+          return vim.fn.fnamemodify(dir, ':~')
+        else
+          -- If there is no current directory (e.g. over ssh), just show the buffer name
+          return vim.api.nvim_buf_get_name(0)
+        end
+      end
+
+      require('oil').setup {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _) return name == '..' or name == '.git' end,
+        },
+        buf_options = {
+          buflisted = false,
+          bufhidden = 'hide',
+        },
+        win_options = {
+          wrap = true,
+          signcolumn = 'yes:2',
+          winbar = '%!v:lua.get_oil_winbar()',
+        },
+        confirmation = {
+          border = 'rounded',
+        },
+      }
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+  { 'refractalize/oil-git-status.nvim', dependencies = { 'stevearc/oil.nvim' }, config = true },
 }
