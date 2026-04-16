1a172df14deb16a913adf4664ce961e0a2cedbb4
Axel Rizo <axelg.rizos@gmail.com>
2026-04-15 21:04:33 -0500

    fix(nvim): update flash keybindings

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -9,8 +9,8 @@ return {
   },
   keys = {
     { 'ñ', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
-    -- { 'Ñ', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
-    { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
+    { 'Ñ', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
+    -- { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
     -- { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
     { '<c-ñ>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
   },
