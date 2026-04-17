8073001651b7830633d9a7687b2296fc51fe27f5
Axel Rizo <axelg.rizos@gmail.com>
2026-04-16 23:33:26 -0500

    feat(nvim): when open telescope remove statusline

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,52 @@
+local function normalize_path(path) return path:gsub('\\', '/') end
+
+local function normalize_cwd() return normalize_path(vim.loop.cwd()) .. '/' end
+
+local function is_subdirectory(cwd, path) return string.lower(path:sub(1, #cwd)) == string.lower(cwd) end
+
+local function split_filepath(path)
+  local normalized_path = normalize_path(path)
+  local normalized_cwd = normalize_cwd()
+  local filename = normalized_path:match '[^/]+$'
+
+  if is_subdirectory(normalized_cwd, normalized_path) then
+    local stripped_path = normalized_path:sub(#normalized_cwd + 1, -(#filename + 1))
+    return stripped_path, filename
+  else
+    local stripped_path = normalized_path:sub(1, -(#filename + 1))
+    return stripped_path, filename
+  end
+end
+
+local function path_display(_, path)
+  local stripped_path, filename = split_filepath(path)
+  if filename == stripped_path or stripped_path == '' then return filename end
+  return string.format('%s ~ %s', filename, stripped_path)
+end
+
+local temp_showtabline
+local temp_laststatus
+
+function _G.global_telescope_find_pre()
+  temp_showtabline = vim.o.showtabline
+  temp_laststatus = vim.o.laststatus
+  vim.o.showtabline = 0
+  vim.o.laststatus = 0
+end
+
+function _G.global_telescope_leave_prompt()
+  vim.o.laststatus = temp_laststatus
+  vim.o.showtabline = temp_showtabline
+end
+
+vim.cmd [[
+  augroup MyAutocmds
+    autocmd!
+    autocmd User TelescopeFindPre lua global_telescope_find_pre()
+    autocmd FileType TelescopePrompt autocmd BufLeave <buffer> lua global_telescope_leave_prompt()
+  augroup END
+]]
+
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   -- By default, Telescope is included and acts as your picker for everything.
